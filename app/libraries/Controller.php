028c7cb2d6ce66b6a5328c7298a96dba6fae3c49
Mohammed Najjar <najjarmedd@gmail.com>
2020-12-19 17:56:51 +0100

    Gestion Url view pages

---
+++ app/libraries/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+//load models and views
+class Controller{
+    public function model($model){
+        //require model file
+        require_once'../app/models/' . $model . '.php';
+        //instantiate model
+        return new $model();
+    }
+
+    //load the view (check for the file)
+    public function view($view, $data = []){
+        if(file_exists('../app/views/' . $view . '.php')){
+            require_once '../app/views/' . $view . '.php';
+
+        } else {
+            die("View does not exist!");
+        }
+    }
+}
